6225a5433b3821f164f4ed82ce90a61d62e44d4a
iqbalaulia <iqbal.developer16@gmail.com>
2022-06-07 11:49:47 +0700

    element button global

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -3,7 +3,9 @@ import Button from "elements/Button";
 export default function Homepage() {
   return (
     <div>
-      <Button>Example</Button>
+      <Button isPrimary isSmall>
+        Sign up
+      </Button>
     </div>
   );
 }
